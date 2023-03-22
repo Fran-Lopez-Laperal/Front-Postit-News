49705923344cbfc98dea4598b47a567730bac43c
Fran <fransanxenxo@gmail.com>
2023-03-22 18:08:15 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,11 @@ function App() {
           <li>{compi}</li>
         </ul>
       ))}
+
+      <h2>Recoradad:</h2>
+     <h3>Siempre pull antes de trabajar</h3>
+     <h3>Trabaja desde tu propia rama, nunca desde Main</h3>
+     <h3>Y sobre todo, vamos a aprender!!!!</h3>
     </div>
   )
 }
