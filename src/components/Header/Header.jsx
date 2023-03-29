322eeb6762ec439973376597351a3fbe24e2cdc6
Fran <fransanxenxo@gmail.com>
2023-03-29 13:05:51 +0200

    add logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import logo from '../assets/logo.png'
 
 import './Header.css'
 
 const Header = () => {
   return (
     <div className='header'>
-      Header
+    <img src={logo} alt="" />
     </div>
   )
 }
