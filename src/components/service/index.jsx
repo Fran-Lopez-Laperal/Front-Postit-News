2dd6282ebce2eaf26db38819fde319d8163021af
Meira81 <guadalupe.alonso.meira@gmail.com>
2023-03-30 17:14:07 +0200

    login+context(AuthContext)+Auth.jsx

--- src/components/service/index.jsx
+++ src/components/service/index.jsx
@@ -15,3 +15,39 @@ export const registerUserService = async ({ name, email, password, bio }) => {
     throw new Error(json.message);
   }
 };
+
+export const loginUserService = async ({email, password}) => {
+  const response = await fetch(`http://localhost:4000/users/login`, {
+  method: "POST",
+  headers: {
+      "Content-Type": "application/json",
+    },
+  body: JSON.stringify({email, password})
+
+  });
+
+  const json = await response.json();
+
+  if (!response.ok) {
+    throw new Error(json.message);
+  }
+
+  return json.data; //es el token
+
+};
+
+export const getMyUserDataService = async ({token}) => {
+  const response= await fetch(`http://localhost:4000/users`, {
+    headers: {
+      Authorization: token
+    },
+  });
+
+  const json = await response.json();
+
+  if(!response.ok){
+    throw new Error(json.message);
+  }
+
+  return json.data;
+};
