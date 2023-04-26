91ff0dbb77bad20c35360a18369647e9971e5214
Meira81 <guadalupe.alonso.meira@gmail.com>
2023-04-26 12:13:46 +0200

    arreglo logo header con onclick

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -19,6 +19,7 @@ const Header = ({ handleCloseClick, setShowNavMovile }) => {
   const handleDeleteFilterNavbar = () => {
     setFilter(false);
   };
+  
 
   return (
     <div className="header">
